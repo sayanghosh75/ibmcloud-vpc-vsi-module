18f6f2e51c0e0ecf61b17fcfe040a071aa180919
Michael Shallcross <m.a.shallcross@gmail.com>
2021-06-30 12:28:49 +1000

    Testing 2 ssh keys

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ data "ibm_is_ssh_key" "ds_key" {
 }
 
 data "ibm_is_ssh_key" "ds_key2" {
-  for_each = toset(var.ssh_key_name2)
-  # for_each = var.ssh_key_name2
-  name = each.value
+  # for_each = toset(var.ssh_key_name2)
+  # name = each.value
+  name = var.ssh_key_name2[*]
 }
 
 data "ibm_is_image" "ds_os" {
